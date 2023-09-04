214161b87fa72ed70e205c29e3498b8eaeb4618b
Tobias Tonndorff <tobias@tonndorff.dk>
2023-09-04 10:25:11 +0200

    more classes

--- src/main/java/entities/Hobby.java
+++ src/main/java/entities/Hobby.java
@@ -1,4 +1,25 @@
 package entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "hobby")
 public class Hobby {
+
+    @Id
+    private int id;
+
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "wikilink")
+    private String wikiLink;
+
+
+
+
 }
