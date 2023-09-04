d91e38d410b72dedaef54470bad2d9962e8dd432
Tobias Tonndorff <cph-tt160@cphbusiness.dk>
2023-09-04 15:39:01 +0200

    added basic user entity

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -1,5 +1,33 @@
 package entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+
+@Table(name = "users")
+@Entity
 public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "email")
+    private String email;
+
+    //add date attributes for: created, lastEdited, birthday
+
+    //add address to user entity
 
 
 }
