f8034b194d0fd17f085c284795856e1a24ba2c00
Tobias Tonndorff <tobias@tonndorff.dk>
2023-09-04 11:27:04 +0200

    configed hibernate and hobby entity

--- src/main/java/entities/Hobby.java
+++ src/main/java/entities/Hobby.java
@@ -1,8 +1,12 @@
 package entities;
 
-import ENUM.Category;
-import ENUM.Type;
+import lombok.*;
 import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 
 @Entity
 @Table(name = "hobby")
@@ -19,11 +23,44 @@ public class Hobby {
     @Column(name = "wikilink")
     private String wikiLink;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "category")
     private Category category;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "type")
-    private Type type;
+    private HobbyType type;
+
+
+    private enum HobbyType {
+        INDENDØRS("Indendørs"),
+        KONKURRENCE("Konkurrence"),
+        KOMBI("Idendørs/Udendørs"),
+        OBSERVATION("Observation"),
+        SAMLER("Samler hobbyer"),
+        UDENDØRS("Udendørs"),
+        NA("---");
+
+        private final String typeString;
+
+        private HobbyType(String typeString) {
+            this.typeString = typeString;
+        }
+    }
+
+    private enum Category {
+        GERNEREL("Generel"),
+        EDUCATIONAL("Educational hobbies"),
+        SAMLINGER("Samler hobbyer"),
+        KONKURRENCE("Konkurrence"),
+        OBSERVATION("Observation");
+
+        private final String categoryString;
+
+        private Category(String categoryString) {
+            this.categoryString = categoryString;
+        }
+    }
 
 
 
