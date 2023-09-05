1e609f5ac4c267be2f831c1a9e6574aad7e227c6
Chris_ziegler <60019587+ChriZZZy@users.noreply.github.com>
2023-09-05 09:22:31 +0200

    User Entity fixed

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -2,6 +2,9 @@ package entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -15,19 +18,50 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private int id;
+    
+    @Column(name = "firstname", nullable = false)
+    private String firstname;
 
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "surname")
+    @Column(name = "surname", nullable = false)
     private String surname;
 
-    @Column(name = "email")
+    @Temporal(TemporalType.DATE)
+    @Column(name = "birthdate")
+    private LocalDate birthdate;
+
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
+
+    @Column(name = "zip", nullable = false)
+    private String zip;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Hobby> hobbies;
+
+    @OneToMany(mappedBy = "phone")
+    private List<PhoneNumber> phones;
+
+    @ManyToOne
+    private Address address;
 
     //add date attributes for: created, lastEdited, birthday
 
     //add address to user entity
-
-
+    public User(String firstname, String surname, LocalDate birthdate, String email, LocalDate createdAt, LocalDate updatedAt, String zip) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.birthdate = birthdate;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.zip = zip;
+    }
 }
