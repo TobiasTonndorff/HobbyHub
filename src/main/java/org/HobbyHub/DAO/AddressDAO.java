d473df0b574d86993984fb06bcf1682b7a87b4e4
Andreas Pihl <54024731+bakkary@users.noreply.github.com>
2023-09-05 12:46:17 +0200

    made save address and update address
    start of CRUD operations

--- src/main/java/org/HobbyHub/DAO/AddressDAO.java
+++ src/main/java/org/HobbyHub/DAO/AddressDAO.java
@@ -1,5 +1,42 @@
 package org.HobbyHub.DAO;
 
-public class AddressDAO {
 
+import jakarta.persistence.EntityManagerFactory;
+
+public class AddressDAO {
+private static EntityManagerFactory emf;
+private static AddressDAO instance;
+
+
+    public static AddressDAO getInstance(EntityManagerFactory _emf){
+        if(instance == null) {
+            emf = _emf;
+            instance = new AddressDAO();
+        }
+    return instance;
+        }
+
+        public void saveAddress(AddressDAO addressDAO){
+        try (var em = emf.createEntityManager()){
+            em.getTransaction().begin();
+            em.persist(addressDAO);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            }
+
+
+        }
+
+        public void updateAddress(AddressDAO addressDAO) {
+            try (var em = emf.createEntityManager()) {
+                em.getTransaction().begin();
+                em.persist(addressDAO);
+                em.getTransaction().commit();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
 }
