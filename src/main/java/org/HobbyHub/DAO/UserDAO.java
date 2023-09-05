45f69bbd133ad8af8f522fd024f7a7457a8106df
Tobias Tonndorff <tobias@tonndorff.dk>
2023-09-05 11:41:51 +0200

    initial

---
+++ src/main/java/org/HobbyHub/DAO/UserDAO.java
@@ -0,0 +1,37 @@
+package org.HobbyHub.DAO;
+import jakarta.persistence.*;
+import org.HobbyHub.entities.User;
+public class UserDAO {
+
+    private static EntityManagerFactory emf;
+    private static UserDAO instance;
+
+    public static UserDAO getInstance(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new UserDAO();
+
+        }
+        return instance;
+    }
+
+    public void saveUser(User user) {
+
+        try(var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUser(User user){
+        try(var em = emf.createEntityManager()){
+
+        }
+    }
+
+
+
+}
